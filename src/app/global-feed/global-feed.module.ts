03ad6d3de9b80126b4fba09534f275cd7ab14640
Admin <1q.expert@gmail.com>
2023-09-14 19:59:14 +0300

    global-feed created

---
+++ src/app/global-feed/global-feed.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GlobalFeedComponent } from './global-feed-component/global-feed.component';
+
+@NgModule({
+  declarations: [GlobalFeedComponent],
+  imports: [CommonModule],
+})
+export class GlobalFeedModule {}
